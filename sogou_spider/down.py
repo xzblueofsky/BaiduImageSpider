c6041eaf410b5be9873e432dd3d43ac553ee9d2d
xinzhao <xinzhao@deepglint.com>
2017-04-13 14:55:37 +0800

    add sogou spider

--- sogou_spider/down.py
+++ sogou_spider/down.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 # @Author: loveNight
 
@@ -13,18 +14,11 @@ import sys
 # 生成网址列表
 def buildUrls(word):
     word = urllib.parse.quote(word)
-    url = r"http://pic.sogou.com/pics?query={word}&mode=1&start={pn}&reqType=ajax&reqFrom=result&tn=0"
-    urls = (url.format(word=word, pn=x) for x in itertools.count(start=0, step=60))
+    #url = r"http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&fp=result&queryWord={word}&cl=2&lm=-1&ie=utf-8&oe=utf-8&st=-1&ic=0&word={word}&face=0&istype=2nc=1&pn={pn}&rn=60"
+    url = r"http://pic.sogou.com/pics?query=test&mode=1&start=240&reqType=ajax&reqFrom=result&tn=0"
+    urls = (url)
+    #urls = (url.format(word=word, pn=x) for x in itertools.count(start=0, step=60))
     return urls
-
-# 解析JSON获取图片URL
-re_url = re.compile(r'"pic_url":"(.*?)"')
-def resolveImgUrl(html):
-    #imgUrls = [decode(x) for x in re_url.findall(html)]
-    print (type(html))
-    imgUrls = re_url.findall(html)
-    print (imgUrls)
-    return imgUrls
 
 def downImg(imgUrl, dirpath, imgName):
     filename = os.path.join(dirpath, imgName)
@@ -40,7 +34,6 @@ def downImg(imgUrl, dirpath, imgName):
     with open(filename, "wb") as f:
         f.write(res.content)
     return True
-
 
 def mkDir(dirName):
     dirpath = os.path.join(sys.path[0], dirName)
@@ -61,7 +54,14 @@ if __name__ == '__main__':
     for url in urls:
         print("正在请求：", url)
         #html = requests.get(url, timeout=10).content.decode('utf-8')
-        html = requests.get(url, timeout=10).text
+        html = requests.get(url, timeout=10).content
+        html = html.rstrip()
+        html = html.replace("\r\n", "")
+        print (html)
+        requ_str = json.load(html, "ISO-8859-1")
+        print (requ_str)
+        #html = html.decode('utf-8') 
+        #html = requests.get(url, timeout=10).content
         imgUrls = resolveImgUrl(html)
         if len(imgUrls) == 0:  # 没有图片则结束
             break
@@ -69,3 +69,4 @@ if __name__ == '__main__':
             if downImg(url, dirpath, str(index) + ".jpg"):
                 index += 1
                 print("已下载 %s 张" % index)
+
